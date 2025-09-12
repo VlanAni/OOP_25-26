4b27be94c280a141a3bfa882ad4c97daac31c10a
VlanAni <VlanAni@users.noreply.github.com>
2025-09-12 12:42:51 +0000

    Deploying to gh-pages from @ VlanAni/OOP@8e1e04aff482c55b897982a570934e6e81504b98 🚀

--- Task_1_1/member-search-index.js
+++ Task_1_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"HeapSort","l":"HeapSort()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HeapSort","l":"sortArr(int[])"}];updateSearchResults();
+memberSearchIndex = [{"p":"<Unnamed>","c":"HeapSort","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
